ecee64dfd3832caddeb806927b97dfc23a6a042c
Karem Cristine <krmcristne@gmail.com>
2020-03-30 23:23:01 -0400

    POST dos atributos email e telefone

--- Funcoes.php
+++ Funcoes.php
@@ -10,11 +10,16 @@ if (isset($_POST['titulo'])) :
     $aviso = new Aviso();
     $aviso->setTitulo($_POST['titulo']);
     $aviso->setDescricao($_POST['descricao']);
+    $aviso->setEmail($_POST['email']);
+    $aviso->setTelefone($_POST['telefone']);
 
     array_push($_SESSION['avisos'], array(
     'titulo' => $aviso->getTitulo(),
-    'descricao' => $aviso->getDescricao()
+    'descricao' => $aviso->getDescricao(),
+    'email' => $aviso->getEmail(),
+    'telefone' => $aviso->getTelefone()
 ));
+ 
 header('Location: /PaineldeAvisos/');
 endif;
 
@@ -26,6 +31,8 @@ function listarAvisos()
     echo "<tr>";
     echo "<th>Titulo</th>";;
     echo "<th>Descrição</th>";
+    echo "<th>Email</th>";;
+    echo "<th>Telefone</th>";
     echo "<th>Ações</th>";
     echo "</tr>";
     foreach($_SESSION['avisos'] as $key => $value)
@@ -33,9 +40,11 @@ function listarAvisos()
         echo "<tr>";
         echo "<td>" . $value['titulo']."</td>";
         echo "<td>" . $value['descricao']."</td>" ;
-        echo "<td><br><a href='?comando=excluir&id=".$key."'class='btn btn-sm btn-danger'>Excluir</a></td>";
+        echo "<td>" . $value['email']."</td>";
+        echo "<td>" . $value['telefone']."</td>" ;
+        echo "<td><a href='?comando=excluir&id=".$key."'class='btn btn-sm btn-danger text-align'>Excluir</a></td>";
         echo "</tr>";
-        
+       
     }
     echo "</table>";
 
